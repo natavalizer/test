93620f534a4da28521b9277320017be200f2c667
natavalizer <natavalizer@gmail.com>
2023-01-31 17:33:02 +0200

    add files

---
+++ operation.py
@@ -0,0 +1,21 @@
+def calculate(number_1, number_2):
+    task = input('Выберите операцию:+, -, *, /')
+    if task == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+    elif task == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+    elif task == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+    elif task == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+    else:
+        print('Такой операции нет:')
+
+
+number_1 = int(input('Введите первое число: '))
+number_2 = int(input('Введите второе число: '))
+calculate(number_1, number_2)
