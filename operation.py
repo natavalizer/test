bfaf4b2cf47eefa89bc40ddaad9a7b170cb62043
natavalizer <natavalizer@gmail.com>
2023-02-02 12:09:25 +0200

    числа

--- operation.py
+++ operation.py
@@ -16,6 +16,6 @@ def calculate(number_1, number_2):
         print('Такой операции нет:')
 
 
-number_1 = int(input('Введите  число: '))
-number_2 = int(input('Введите  число: '))
+number_1 = int(input('Введите число: '))
+number_2 = int(input('Введите число: '))
 calculate(number_1, number_2)
